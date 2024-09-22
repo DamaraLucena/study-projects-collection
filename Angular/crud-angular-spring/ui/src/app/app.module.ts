39a00068aee3daa2d52b4ee88dd911662ef35dcd
damaralucena <damara.ti.dev@gmail.com>
2024-09-22 17:51:46 -0300

    feat: Criando tabela de cursos e ajustando modulo para o angular material

--- Angular/crud-angular-spring/ui/src/app/app.module.ts
+++ Angular/crud-angular-spring/ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MatToolbar} from "@angular/material/toolbar";
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,6 +15,7 @@ import {MatToolbar} from "@angular/material/toolbar";
     BrowserModule,
     AppRoutingModule,
     MatToolbar
+
   ],
   providers: [
     provideAnimationsAsync('noop')
