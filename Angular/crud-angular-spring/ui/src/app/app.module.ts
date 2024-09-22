4f22ba3e87ff54da4e5cafeb25a4b1d91b72bb9d
damaralucena <damara.ti.dev@gmail.com>
2024-09-22 15:00:45 -0300

    feat: Criando o roteamento com lazy loading

--- Angular/crud-angular-spring/ui/src/app/app.module.ts
+++ Angular/crud-angular-spring/ui/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MatToolbar} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
@@ -13,7 +13,7 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    MatToolbar
   ],
   providers: [
     provideAnimationsAsync('noop')
